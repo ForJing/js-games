b17061380dc61b7d407ed9e04c90911671a982eb
forjing <xiaodoubao1994@gmail.com>
2018-09-24 16:45:14 +0800

    add ball

--- src/index.js
+++ src/index.js
@@ -2,3 +2,37 @@ import "./style.css";
 
 const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext("2d");
+
+const fps = 60;
+
+const ballRadius = 10;
+
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+
+var dx = 2;
+var dy = -2;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+
+  if (x + ballRadius + dx > canvas.width || x - ballRadius + dx < 0) {
+    dx = -dx;
+  }
+  if (y + dy + ballRadius > canvas.height || y - ballRadius + dy < 0) {
+    dy = -dy;
+  }
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 1000 / fps);
